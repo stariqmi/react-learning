30a1e7220bca65e7f76d838c62d12bc1740fcabd
Salman Tariq Mirza <salmantariq_mirza@apple.com>
2016-06-04 18:57:36 -0700

    learning react starter

--- App.js
+++ App.js
@@ -3,17 +3,9 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
     render() {
-        return <div>{this.props.txt}</div>;
+        return <div>Hello World</div>
     }
 }
 
-App.propTypes = {
-    txt: React.PropTypes.string,
-    // name: React.PropTypes.number.isRequired
-};
-
-ReactDOM.render(
-    <App txt="this is the props text" />,
-    document.getElementById('app')
-);
+export default App;
 
--- main.js
+++ main.js
@@ -2,3 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App.js';
+
+ReactDOM.render(<App age={7} />, document.getElementById('app'));
